1bc626a6df9d358e10065f4372a307077641202c
unicodeixixixi <109984109+unicodeixixixi@users.noreply.github.com>
2023-05-18 21:55:05 +0200

    Create C++ script for Windows window creation using WinAPI
    This commit adds a C++ script that creates a window in Windows using the WinAPI. The script utilizes the WinAPI functions to register a window class, create a window, and handle messages received by the window. It provides a basic window with a title and default painting functionality. Developers can use this script as a starting point for creating custom Windows applications that require window creation and message handling.

---
+++ windows/main.cpp
@@ -0,0 +1,79 @@
+// Boblox main Client
+// 2d Physics Engine
+// Open Source
+// Feel free to edit - for Boblox Site
+
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // Step 1: Register the window class
+
+    const wchar_t CLASS_NAME[] = L"MyWindowClass";
+
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Step 2: Create the window
+
+    HWND hwnd = CreateWindowEx(
+        0,                      // Optional window styles
+        CLASS_NAME,             // Window class name
+        L"Window Title",        // Window title
+        WS_OVERLAPPEDWINDOW,    // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Step 3: Run the message loop
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+            EndPaint(hwnd, &ps);
+        }
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
