018c41b1513a013b96104acd3b733082c63edfb3
unicodeixixixi <109984109+unicodeixixixi@users.noreply.github.com>
2023-05-20 13:30:01 +0200

    fix client
    window couldn't open somehow

--- windows/main.cpp
+++ windows/main.cpp
@@ -1,32 +1,36 @@
-// Boblox main Client
-// 2d Physics Engine
-// Open Source
-// Feel free to edit - for Boblox Site
+#include <Windows.h>
 
-#include <windows.h>
+// Step 1: Declare the window procedure
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
+// Step 2: Create the window
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    // Step 1: Register the window class
-
-    const wchar_t CLASS_NAME[] = L"MyWindowClass";
+    // Step 3: Register the window class
+    const char* CLASS_NAME = "MyWindowClass";
 
     WNDCLASS wc = {};
-
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
 
     RegisterClass(&wc);
 
-    // Step 2: Create the window
-
+    // Step 4: Create the window
     HWND hwnd = CreateWindowEx(
         0,                      // Optional window styles
         CLASS_NAME,             // Window class name
-        L"Window Title",        // Window title
+        "My Window",            // Window title
         WS_OVERLAPPEDWINDOW,    // Window style
 
         // Size and position
@@ -39,14 +43,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     );
 
     if (hwnd == NULL)
-    {
         return 0;
-    }
 
+    // Step 5: Show and update the window
     ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
 
-    // Step 3: Run the message loop
-
+    // Step 6: Message loop
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0))
     {
@@ -55,25 +58,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     }
 
     return msg.wParam;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg)
-    {
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-
-        case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-            EndPaint(hwnd, &ps);
-        }
-        return 0;
-    }
-
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
